e4ab28a6ab330ea7f3be0149390d7fe4205e89f1
dipsonu10 <dipsonu10@hotmail.com>
2021-08-23 12:10:07 +0530

    Completed the lab03

--- Lab02/a9.c
+++ Lab02/a9.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
     scanf("%d %d", &m, &n);
     printf("Enter the invalid number: ");
     int inv;
-    scanf("%d",&inv);
+    scanf("%d", &inv);
     int **a = (int **)malloc(m * sizeof(int *));
     for (int i = 0; i < m; i++)
         a[i] = (int *)malloc(n * sizeof(int));
@@ -18,35 +18,51 @@ int main(int argc, char const *argv[])
         for (int j = 0; j < n; j++)
             scanf("%d", &a[i][j]);
 
-    int c=0;
+
+    int **res = (int **)malloc(m * sizeof(int *));
     for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-            if(a[i][j]!=inv){
-                c++;
-            }
+        res[i] = (int *)malloc(n * sizeof(int));
 
-    int **res = (int **)malloc(c * sizeof(int *));
-    for (int i = 0; i < c; i++)
-        res[i] = (int *)malloc(c * sizeof(int));
+    for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+            res[i][j]=inv;
 
-    int row=0, col=0;
+    int row = 0, col = 0;
+    int flag=0;
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
         {
-            
-            if(a[i][j]!=inv)
+            if (a[i][j] != inv)
             {
-                res[row][col]=a[i][j];
+                if ((row == m || col == n) && !flag)
+                {
+                    flag=1;
+                    row = 0;
+                    col = 1;
+                    //shift to right
+                    res[row][col] = a[i][j];
+                }
+                else if((row == m || col == n) && flag){
+                    flag=2;
+                    row = 1;
+                    col = 0;
+                    //shift to left
+                    res[row][col] = a[i][j];
+                }
+                else
+                    res[row][col] = a[i][j];
+
                 row++;
                 col++;
             }
         }
-    
-    for(int i=0;i<c;i++)
+
+    for (int i = 0; i < m; i++)
     {
-        for(int j=0;j<c;j++){
-            if(i==j)
-                printf("%d ",res[i][j]);
+        for (int j = 0; j < n; j++)
+        {
+            if (res[i][j]!=inv)
+                printf("%d ", res[i][j]);
             else
                 printf("* ");
         }
