67e0af2cae5fb0e4a45d816f441ea3865bb130d1
DipankarDas <dipsonu10@hotmail.com>
2021-12-04 11:41:33 +0530

    PracticeQuestions: Tree1.c
    PATCH (1/3): Completed the iterative of traversal (pre/post/in)

--- PracticeQuestions/Tree1.c
+++ PracticeQuestions/Tree1.c
@@ -5,54 +5,124 @@
 // left and right children of every node.
 #include <stdio.h>
 #include <stdlib.h>
-#include "../CustomDS/QueueI.h"
+#include "../CustomDS/Stack.h"
 
+typedef struct n
+{
+    int data;
+    struct n *left, *right;
+} tree;
 
-typedef struct n{
-	int data;
-	struct n*left,*right;
-}tree;
-
-
-tree* allocateMemory(int k){
-	tree* t = (tree*)malloc(sizeof(tree));
-	t->data = k;
-	t->left = t->right = NULL;
-	return t;
+tree *allocateMemory(int k)
+{
+    tree *t = (tree *)malloc(sizeof(tree));
+    t->data = k;
+    t->left = t->right = NULL;
+    return t;
 }
 
-void inorderRec(tree* root){
-  if(root){
-    inorderRec(root->left);
-    printf("%d ", root->data);
-    inorderRec(root->right);
-  }
+void inorderRec(tree *root)
+{
+    if (root){
+        inorderRec(root->left);
+        printf("%d ", root->data);
+        inorderRec(root->right);
+    }
 }
 
-void preorderRec(tree* root){
-  if(root){
-    printf("%d ", root->data);
-    preorderRec(root->left);
-    preorderRec(root->right);
-  }
+void preorderRec(tree *root)
+{
+    if (root){
+        printf("%d ", root->data);
+        preorderRec(root->left);
+        preorderRec(root->right);
+    }
 }
 
-void postorderRec(tree* root){
-  if(root){
-    postorderRec(root->left);
-    postorderRec(root->right);
-    printf("%d ", root->data);
-  }
+void postorderRec(tree *root)
+{
+    if (root){
+        postorderRec(root->left);
+        postorderRec(root->right);
+        printf("%d ", root->data);
+    }
 }
 
-void postorderIter(tree* root){}
-void inorderIter(tree* root){}
-void preorderIter(tree* root){}
+/**
+ * @def Push directly root node two times while traversing to the left. While poping if you find stack top() is same as root then go for root->right else print root.
+ * @param root root of the tree
+ */
+void postorderIter(tree *root)
+{
+    // left-right-root
+    Stack *stk = NULL;
+    while (1)
+    {
+        while(root) {
+            push(&stk, root);
+            push(&stk, root);
+            root = root->left;
+        }
+         
+        // Check for empty stack
+        if(isEmpty(&stk))
+            break;
+        root = (tree *)peek(&stk);
+        pop(&stk);
 
+        if(!isEmpty(&stk) && (tree *)peek(&stk) == root) 
+            root = root->right;
 
-int main(int argc, char **argv) {
-  tree* root = 0;
-  /**
+        else {
+            printf("%d ",root->data); 
+            root = NULL;
+        }
+    }
+    printf("\n\n");
+}
+
+void inorderIter(tree *root)
+{
+    // left - root - right
+    // always go towards the left
+    tree *iter = root;
+    Stack* stk = NULL;
+    while(iter || !isEmpty(&stk)){
+        while(iter){
+            push(&stk, iter);
+            iter = iter->left;
+        }
+
+        iter = (tree *)peek(&stk);
+        pop(&stk);
+        printf("%d ", iter->data);
+        iter = iter->right;
+    }
+    printf("\n\n");
+}
+
+void preorderIter(tree *root)
+{
+    // root-left-right
+    Stack *stk = NULL;
+    push(&stk, root);
+    while(!isEmpty(&stk)){
+        tree *ii = (tree*)peek(&stk);
+        printf("%d ", ii->data);
+        pop(&stk);
+
+        if(ii->right)
+            push(&stk, ii->right);
+        if(ii->left)
+            push(&stk, ii->left);
+    }
+    printf("\n\n");
+}
+
+int main(int argc, char **argv)
+{
+    tree *root = 0;
+    /**
    *    1
    *   / \
    *  2   3
@@ -61,14 +131,30 @@ int main(int argc, char **argv) {
    *   \
    *   7
    */
-	root = allocateMemory(1);
-	root->left = allocateMemory(2);
-	root->right = allocateMemory(3);
-	root->left->right = allocateMemory(5);
-	root->left->left = allocateMemory(4);
-	root->left->right->right = allocateMemory(7);
-	root->right->left= allocateMemory(6);
-  
-  remove(argv[0]);
-  return EXIT_SUCCESS;
+    root = allocateMemory(1);
+    root->left = allocateMemory(2);
+    root->right = allocateMemory(3);
+    root->left->right = allocateMemory(5);
+    root->left->left = allocateMemory(4);
+    root->left->right->right = allocateMemory(7);
+    root->right->left = allocateMemory(6);
+
+    printf("~~~ inorder method ~~~\n");
+    inorderRec(root);
+    printf("\n");
+    inorderIter(root);
+
+    printf("~~~ preorder method ~~~\n");
+    preorderRec(root);
+    printf("\n");
+    preorderIter(root);
+
+    printf("~~~ postorder method ~~~\n");
+    postorderRec(root);
+    printf("\n");
+    postorderIter(root);
+
+
+    remove(argv[0]);
+    return EXIT_SUCCESS;
 }
