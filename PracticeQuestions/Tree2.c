74fa171f65128663658bfb41b5ff8d1a5284be8e
DipankarDas <dipsonu10@hotmail.com>
2021-12-04 19:29:35 +0530

    PracticeQuestions: Tree2.cpp
    Completed the Copy tree and the psedo code for preorder & postorder in threaded

--- PracticeQuestions/Tree2.c
+++
@@ -1,8 +0,0 @@
-/*
- Suppose ROOT A points to a binary tree T1. Write the pseudo code which
-makes a copy T2 of the tree T1 using ROOT B as pointer
- Write a function that traverses a threaded binary tree in postorder. What are
-the time and space requirement of your method?
- Write a function that traverses a threaded binary tree in preorder. What are
-the time and space requirement of your method?
-*/
